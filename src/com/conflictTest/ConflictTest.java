1a45074e2bfea1ebafb783b7a8760616b051a904
Jung-HOO <ljh@gmail.com>
2020-04-24 11:01:47 +0900

    test commit

--- src/com/conflictTest/ConflictTest.java
+++ src/com/conflictTest/ConflictTest.java
@@ -2,4 +2,16 @@ package com.conflictTest;
 
 public class ConflictTest {
 
+	public static void main(String args[]) {
+
+		// char array
+		char[] charArray = new char[] { 'N', 'i', 'n', 'e', 'o', 'n', 'e' };
+
+		
+
+		String str = new String(charArray);
+		System.out.println("Char array converted to String: " + str);
+
+	}
+
 }
